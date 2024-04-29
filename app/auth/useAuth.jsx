fef41d92bd0c8de1f0518e094eeaac3b28864160
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-04-29 15:04:41 -0400

    fixed bug

--- app/auth/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../context/user';
-
-export const useAuth = () => {
-	const { user } = useContext(UserContext);
-
-	return { user };
-};
