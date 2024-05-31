aa976710efd8800333c983f3128c03e06c30db5d
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-05-31 16:45:12 -0400

    added functionality to chart, color labels

--- components/pages/trends.tsx
+++ components/pages/trends.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import TrendLineChart from '../home/trendLineChart';
+'use client';
+import React, { useEffect, useState } from 'react';
+import TrendLineChart, { singleMood } from '../home/trendLineChart';
+import { getUser } from '../utils/serverFunctions';
+import { useAuth } from '@/app/context/UserProvider';
+import { formatValueTypeToYYYYMMDD } from '../utils/reusableFunctions';
 
 const Trends = () => {
+	const { user, isUpdated } = useAuth();
+	const [moods, setMoods] = useState<singleMood[]>([]);
+	useEffect(() => {
+		if (user) {
+			getUser(user.uid);
+		}
+	}, [user, isUpdated]);
+
+	const [selectedFilters, setSelectedFilters] = useState({
+		Rainbow: false,
+		Sunny: false,
+		Cloudy: false,
+		Rainy: false,
+		Stormy: false,
+	});
+
+	useEffect(() => {
+		if (user) {
+			getUser(user.uid).then((userData) => {
+				if (userData && userData.moods) {
+					console.log(userData.moods);
+					setMoods(userData.moods);
+				}
+			});
+		}
+	}, [user, isUpdated]);
 	return (
 		<div>
 			<h1>Trends</h1>
 			<p>This is the trends page.</p>
-			<TrendLineChart />
+			{moods && moods.length > 0 && <TrendLineChart moods={moods} />}
 		</div>
 	);
 };
