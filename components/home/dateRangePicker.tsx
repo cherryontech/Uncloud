a2419f26c9d947bc3f10e7340d0f9750743b0967
paola4 <arevalo@ualberta.ca>
2024-06-07 13:07:26 -0600

    Make dropdown close on click outside, change caret

--- components/home/dateRangePicker.tsx
+++ components/home/dateRangePicker.tsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { DateRangePicker, DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import { Calendar, CaretLeft, CaretRight } from '@phosphor-icons/react';
+import {
+	Calendar,
+	CaretLeft,
+	CaretRight,
+	CaretUp,
+	CaretDown,
+} from '@phosphor-icons/react';
 import '/app/styles/dateRangePicker.css';
 import Image from 'next/image';
 
@@ -23,6 +29,8 @@ const CustomDateRangePicker: React.FC<CustomDateRangePickerProps> = ({
 	setState,
 }) => {
 	const [open, setOpen] = useState(false);
+	const dropdownRef = useRef(null);
+	const buttonRef = useRef(null);
 
 	useEffect(() => {
 		const nextButton = document.querySelector('.rdrNextButton i');
@@ -40,39 +48,75 @@ const CustomDateRangePicker: React.FC<CustomDateRangePickerProps> = ({
 			);
 		}
 
+		// Replace the style of the day labels
 		const dayLabels = document.querySelectorAll('.rdrWeekDay');
 		const newDayLabels = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
 		dayLabels.forEach((label, index) => {
 			label.textContent = newDayLabels[index];
 		});
+
+		const handleClickOutside = (event: { target: any }) => {
+			if (
+				dropdownRef.current &&
+				buttonRef.current &&
+				!(dropdownRef.current as unknown as HTMLElement).contains(
+					event.target
+				) &&
+				!(buttonRef.current as unknown as HTMLElement).contains(event.target)
+			) {
+				setOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		// Clean up event listener on unmount
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
 	}, [open]);
 
 	return (
-		<div className='relative flex items-end justify-end '>
+		<div className='relative flex items-end justify-end'>
 			<div className='relative w-fit'>
 				<button
-					className='flex items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-gray-600 shadow-sm '
+					ref={buttonRef}
+					className='flex min-w-[21rem] items-center justify-between gap-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-[#706F6F] shadow-sm'
 					onClick={() => setOpen(!open)}
 				>
-					<div className='w-[1.25rem]'>
-						<Image
-							src='/CalendarBlank.svg'
-							alt='Calendar'
-							width={20}
-							height={20}
-						/>
+					<div className='flex flex-row items-center justify-start gap-2'>
+						<div className='w-[1.25rem]'>
+							<Image
+								src='/CalendarBlank.svg'
+								alt='Calendar'
+								width={20}
+								height={20}
+							/>
+						</div>
+						<span className='text-sm'>
+							{state[0].startDate.toLocaleDateString(undefined, {
+								month: 'long',
+								day: 'numeric',
+								year: 'numeric',
+							})}{' '}
+							-{' '}
+							{state[0].endDate.toLocaleDateString(undefined, {
+								month: 'long',
+								day: 'numeric',
+								year: 'numeric',
+							})}
+						</span>
 					</div>
-					<span>
-						{state[0].startDate.toDateString()} -{' '}
-						{state[0].endDate.toDateString()}
-					</span>
 					<div className='w-[1.25rem]'>
-						<Image src='/CaretDown.svg' alt='Calendar' width={20} height={20} />
+						{open ? <CaretUp /> : <CaretDown />}
 					</div>
 				</button>
 				{open && (
-					<div className='absolute right-0 top-12 z-10 flex w-full flex-col items-end rounded-xl border  border-gray-300 bg-white p-6 shadow-md'>
+					<div
+						ref={dropdownRef}
+						className='absolute right-0 top-12 z-10 flex w-full flex-col items-end rounded-xl border border-gray-300 bg-white p-6 shadow-md'
+					>
 						<div className='mb-2 w-full text-[0.75rem] font-semibold text-[#D9D9D9]'>
 							Show reports for
 						</div>
