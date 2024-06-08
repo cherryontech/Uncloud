1f48194b9916d5497c7a3f6a338749b20e0ec378
paola4 <arevalo@ualberta.ca>
2024-06-08 14:31:09 -0600

    Update style

--- components/pages/profile.tsx
+++ components/pages/profile.tsx
@@ -1,29 +1,9 @@
-'use client';
-import React, {
-	useEffect,
-	useState,
-	ChangeEvent,
-	FormEvent,
-	useRef,
-} from 'react';
-
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import { toast } from 'sonner';
 import { auth, db, storage } from '@/app/firebase';
 import { updateProfile } from 'firebase/auth';
-import {
-	CitySelect,
-	CountrySelect,
-	StateSelect,
-	LanguageSelect,
-} from 'react-country-state-city';
-import 'react-country-state-city/dist/react-country-state-city.css';
-import {
-	doc,
-	DocumentReference,
-	getDoc,
-	updateDoc,
-	where,
-} from 'firebase/firestore';
+import { GetCountries, GetState } from 'react-country-state-city';
+import { doc, DocumentReference, getDoc, updateDoc } from 'firebase/firestore';
 import {
 	getDownloadURL,
 	ref as storageRef,
@@ -37,6 +17,19 @@ import { getUser } from '../utils/serverFunctions';
 import { useRouter } from 'next/navigation';
 import '/app/styles/profile.css';
 import CustomSelectDropdown from '../shared/customSelectDropdown';
+import DropdownInput from '../shared/DropdownInput';
+import { CheckCircle } from '@phosphor-icons/react';
+import '/app/styles/customSelect.css';
+
+interface Country {
+	id: number;
+	name: string;
+}
+
+interface State {
+	id: number;
+	name: string;
+}
 
 interface UserDataForm {
 	displayName: string;
@@ -45,7 +38,11 @@ interface UserDataForm {
 	photoURL: string;
 }
 
-const Profile: React.FC = () => {
+interface ProfileProps {
+	mobile: boolean;
+}
+
+const Profile: React.FC<ProfileProps> = ({ mobile, ...props }) => {
 	const [userDataForm, setUserDataForm] = useState<UserDataForm>({
 		displayName: '',
 		photoURL: '',
@@ -57,11 +54,12 @@ const Profile: React.FC = () => {
 	const [error, setError] = useState<string | null>(null);
 	const [userPopData, setUserPopData] = useState<any | null>(null);
 	const { user, updateData, refetchUser } = useAuth();
-	const [countryObj, setCountryObj] = useState({ id: 0, name: '' });
-	const [stateObj, setStateObj] = useState({ id: 0, name: '' });
+	const [countryObj, setCountryObj] = useState<Country>({ id: 0, name: '' });
+	const [stateObj, setStateObj] = useState<State>({ id: 0, name: '' });
+	const [countryOptions, setCountryOptions] = useState<string[]>([]);
+	const [stateOptions, setStateOptions] = useState<string[]>([]);
 
 	const router = useRouter();
-	const stateSelectDivRef = useRef<HTMLDivElement | null>(null);
 
 	useEffect(() => {
 		if (user) {
@@ -78,12 +76,21 @@ const Profile: React.FC = () => {
 					setCountryObj(userData?.location?.country);
 				userData?.location?.state && setStateObj(userData?.location?.state);
 			});
+
+			GetCountries().then((result: Country[]) => {
+				setCountryOptions(result.map((country) => country.name));
+			});
 		}
 	}, [user]);
 
-	if (!user) {
-		return null;
-	}
+	useEffect(() => {
+		if (countryObj.id) {
+			GetState(countryObj.id).then((result: State[]) => {
+				setStateOptions(result.map((state) => state.name));
+			});
+		}
+	}, [countryObj.id]);
+
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
 		setUserDataForm((prevState) => ({
@@ -110,11 +117,11 @@ const Profile: React.FC = () => {
 				imageUrl = await getDownloadURL(snapshot.ref);
 			}
 
-			const userDocRef = doc(db, 'authUsers', user.uid);
+			const userDocRef = doc(db, 'authUsers', user!.uid);
 			const userDocSnap = await getDoc(userDocRef);
 
 			if (userDocSnap.exists()) {
-				await updateProfile(user, {
+				await updateProfile(user!, {
 					displayName: displayName,
 					photoURL: imageUrl,
 				});
@@ -133,10 +140,16 @@ const Profile: React.FC = () => {
 				return;
 			}
 			router.refresh();
-			toast.success('User Info Updated Successfully');
+			toast('Success', {
+				style: { color: '#2c2c2c' },
+				className: 'my-classname',
+				description: 'User profile updated successfully.',
+				duration: 5000,
+				icon: <CheckCircle size={16} />,
+			});
 		} catch (error: any) {
 			console.log(error);
-			toast.error('Error updating user info: ' + error.message);
+			toast.error('Error updating user information: ' + error.message);
 			setError(error.message);
 		}
 	};
@@ -148,13 +161,29 @@ const Profile: React.FC = () => {
 		'Prefer not to say',
 		'Other',
 	];
+
+	if (!user) {
+		return null;
+	}
+
 	return (
-		<div className=' min-h-[80vh] w-full rounded-2xl border border-[#DEE9F5] bg-white'>
-			<div className='w-full rounded-2xl px-[6rem] py-[6rem] '>
+		<div
+			className={`w-full  ${!mobile ? 'rounded-2xl border border-[#DEE9F5] bg-white' : '!h-fit'}`}
+		>
+			<div className={`w-full rounded-2xl px-[6rem] py-[6rem] `}>
 				<form onSubmit={handleUserData} className='space-y-4'>
-					<div className='flex w-full flex-col items-start justify-between gap-[1.88rem] rounded-xl border border-[#D9D9D9] px-[6.62rem] py-[1.63rem]'>
-						<span className='w-full text-xl font-semibold'> Profile Image</span>
-						<div className='flex w-full flex-row justify-between'>
+					<div
+						className={`flex w-full flex-col items-start justify-between gap-[1.88rem] rounded-xl border border-[#D9D9D9] px-[6.62rem] py-[1.63rem] ${mobile ? 'items-center' : ''}`}
+					>
+						<span
+							className={`w-full text-xl font-semibold ${mobile ? 'items-center justify-center text-center' : ''}`}
+						>
+							{' '}
+							Profile Image
+						</span>
+						<div
+							className={`flex w-full justify-between ${mobile ? 'flex-col justify-start gap-4' : 'flex-row'}`}
+						>
 							<div className='flex flex-col items-center justify-center'>
 								<div className='h-[7.3125rem] w-[7.3125rem]'>
 									{imageUpload ? (
@@ -187,7 +216,7 @@ const Profile: React.FC = () => {
 
 							<div className='flex items-center justify-center gap-4'>
 								<div
-									className='bg-grayBackground text-grayTextColor w-fit cursor-pointer rounded-full px-6 py-[0.625rem] !font-bold leading-6 hover:bg-[#DEE9F5] hover:text-blueColor'
+									className='bg-grayBackground text-grayTextColor w-fit cursor-pointer rounded-full px-6 py-[0.625rem] text-sm  !font-bold leading-6 hover:bg-[#DEE9F5] hover:text-blueColor'
 									onClick={() => {
 										setUserDataForm((prevState) => ({
 											...prevState,
@@ -218,15 +247,25 @@ const Profile: React.FC = () => {
 							</div>
 						</div>
 					</div>
-					<div className='flex w-full flex-col items-start justify-between gap-[1.88rem] rounded-xl border border-[#D9D9D9] px-[6.62rem] py-[1.63rem]'>
-						<span className='w-full gap-[1.88rem] text-xl font-semibold'>
+					<div
+						className={`flex w-full flex-col items-start justify-between gap-[1.88rem] rounded-xl border border-[#D9D9D9] px-[6.62rem] py-[1.63rem]`}
+					>
+						<span
+							className={`w-full gap-[1.88rem] text-xl font-semibold ${mobile ? 'text-center' : ''}`}
+						>
 							{' '}
 							Personal Information
 						</span>
-						<div className='flex flex-col gap-[1rem]'>
-							<div className='flex w-[39rem] flex-row justify-between'>
-								<label className='text-sm font-bold text-[#706f6f]'>Name</label>
-								<div className='w-80'>
+						<div
+							className={`flex flex-col gap-[1rem] ${mobile ? 'w-full items-center' : 'w-[70%]'}`}
+						>
+							<div
+								className={`flex justify-between ${mobile ? 'w-fit flex-col gap-2' : 'flex-row'}`}
+							>
+								<label className='w-32 text-sm font-bold text-[#706f6f]'>
+									Name
+								</label>
+								<div className={`${mobile ? 'w-80' : 'w-80'}`}>
 									<CustomInput
 										name={'displayName'}
 										handleChange={handleChange}
@@ -236,18 +275,21 @@ const Profile: React.FC = () => {
 									/>
 								</div>
 							</div>
-							<div className='flex w-[39rem] flex-row justify-between'>
-								<label className='text-sm font-bold text-[#706f6f]'>
+							<div
+								className={`flex flex-row justify-between ${mobile ? 'w-fit flex-col gap-2' : 'flex-row'}`}
+							>
+								<label className='w-32 text-sm font-bold text-[#706f6f] '>
 									Gender
 								</label>
-								<div className='w-80'>
+								<div className={`${mobile ? 'w-80' : 'w-80'}`}>
 									<CustomSelectDropdown
-										onChange={(e) =>
+										onChange={(selectedOption) => {
 											setUserDataForm((prevState) => ({
 												...prevState,
-												gender: e.target.value,
-											}))
-										}
+												gender: selectedOption,
+											}));
+											setChangeFormUserData(true);
+										}}
 										placeholder='Select'
 										name='gender'
 										options={genderOptions}
@@ -255,11 +297,13 @@ const Profile: React.FC = () => {
 									/>
 								</div>
 							</div>
-							<div className='flex w-[39rem] flex-row justify-between'>
-								<label className='text-sm font-bold text-[#706f6f]'>
+							<div
+								className={`flex flex-row justify-between ${mobile ? 'w-fit flex-col gap-2' : 'flex-row'}`}
+							>
+								<label className='w-32 text-sm font-bold text-[#706f6f] '>
 									Career Status
 								</label>
-								<div className='w-80'>
+								<div className={`${mobile ? 'w-80' : 'w-80'}`}>
 									<CustomInput
 										name={'careerStatus'}
 										handleChange={handleChange}
@@ -269,39 +313,66 @@ const Profile: React.FC = () => {
 									/>
 								</div>
 							</div>
-							<div className='flex w-[39rem] flex-row justify-between'>
-								<label className='text-sm font-bold text-[#706f6f]'>
+							<div
+								className={`flex flex-row justify-between ${mobile ? ' w-fit flex-col gap-2' : 'flex-row'}`}
+							>
+								<label className='w-32 text-sm font-bold text-[#706f6f]'>
 									Location
 								</label>
-								<div className='w-80 space-y-2'>
-									<CountrySelect
-										defaultValue={countryObj}
-										onChange={(e: any) => {
-											setCountryObj({ id: e.id, name: e.name });
-											setChangeFormUserData(true);
-											setStateObj({ id: 0, name: '' });
+								<div className={`space-y-2 ${mobile ? 'w-80' : 'w-80'}`}>
+									<CustomSelectDropdown
+										onChange={(selectedOption) => {
+											const selectedCountry = countryOptions.find(
+												(country) => country === selectedOption
+											);
+											if (selectedCountry) {
+												const countryIndex =
+													countryOptions.indexOf(selectedCountry);
+												GetCountries().then((result: Country[]) => {
+													setCountryObj({
+														id: result[countryIndex].id,
+														name: selectedOption,
+													});
+												});
+												setChangeFormUserData(true);
+												setStateObj({ id: 0, name: '' });
+											}
 										}}
-										containerClassName='w-full'
-										placeHolder='Select Country'
-										showFlag={false}
+										placeholder='Select Country'
+										name='country'
+										options={countryOptions}
+										value={countryObj.name}
 									/>
 
-									<StateSelect
-										defaultValue={stateObj}
-										countryid={countryObj.id}
-										containerClassName='w-full'
-										onChange={(e: any) => {
-											setStateObj({ id: e.id, name: e.name });
-											setChangeFormUserData(true);
+									<CustomSelectDropdown
+										onChange={(selectedOption) => {
+											const selectedState = stateOptions.find(
+												(state) => state === selectedOption
+											);
+											if (selectedState) {
+												const stateIndex = stateOptions.indexOf(selectedState);
+												GetState(countryObj.id).then((result: State[]) => {
+													setStateObj({
+														id: result[stateIndex].id,
+														name: selectedOption,
+													});
+												});
+												setChangeFormUserData(true);
+											}
 										}}
-										placeHolder='Select State'
+										placeholder='Select State'
+										name='state'
+										options={stateOptions}
+										value={stateObj.name}
 									/>
 								</div>
 							</div>
 						</div>
-						<div className='mr-10 flex w-full items-end justify-end space-x-4'>
+						<div
+							className={`mr-10 flex w-full items-end space-x-4 ${mobile ? 'justify-center' : 'justify-end'}`}
+						>
 							<button
-								className='bg-grayBackground text-grayTextColor w-fit cursor-pointer rounded-full px-6 py-[0.625rem] !font-bold leading-6 hover:bg-[#DEE9F5] hover:text-blueColor'
+								className='bg-grayBackground text-grayTextColor w-fit cursor-pointer rounded-full px-6 py-[0.625rem] text-sm !font-bold leading-6 hover:bg-[#DEE9F5] hover:text-blueColor'
 								type='button'
 								onClick={() => {
 									setUserDataForm({ ...userPopData });
--- components/shared/customSelectDropdown.tsx
+++ components/shared/customSelectDropdown.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import '/app/styles/customSelectDropdown.css';
+import React, { useState, useEffect, useRef } from 'react';
+import '/app/styles/customSelect.css';
 
 interface DropdownProps {
 	options: string[];
 	value: string;
-	onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+	onChange: (option: string) => void;
 	placeholder: string;
 	name: string;
 }
@@ -16,58 +16,66 @@ const CustomSelectDropdown: React.FC<DropdownProps> = ({
 	placeholder,
 	name,
 }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const dropdownRef = useRef<HTMLDivElement>(null);
+
+	const handleSelect = (option: string) => {
+		onChange(option);
+		setIsOpen(false);
+	};
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			dropdownRef.current &&
+			!dropdownRef.current.contains(event.target as Node)
+		) {
+			setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
 	return (
-		<div className='relative inline-block w-full'>
-			<select
-				name={name}
-				value={value}
-				onChange={onChange}
-				className={[
-					'dropdown-placeholder',
-					'block',
-					'w-full',
-					'rounded-md',
-					'border',
-					'text-gray',
-					'border-[#D9D9D9]',
-					'bg-white',
-					'appearance-none',
-					'p-2',
-					'pl-3',
-					'pr-10',
-					'text-sm',
-					'font-normal',
-					'hover:border-[#2D81E0]',
-					'focus:border-[#2D81E0]',
-					'focus:outline-none',
-					'focus:ring',
-					'focus:ring-[#DEE9F5]',
-				].join(' ')}
+		<div className='custom-dropdown' ref={dropdownRef}>
+			<div
+				className='custom-dropdown__selected !p-2 !pl-3 !pr-10 hover:border-[#2D81E0] focus:border-[#2D81E0] focus:outline-[#2D81E0] focus:ring focus:ring-[#DEE9F5]'
+				onClick={() => setIsOpen(!isOpen)}
 			>
-				<option value='' disabled className='dropdown'>
-					{placeholder}
-				</option>
-				{options.map((option) => (
-					<option key={option} value={option} className='dropdown'>
-						{option}
-					</option>
-				))}
-			</select>
-			<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2'>
-				<svg
-					className='h-5 w-5'
-					fill='none'
-					stroke='currentColor'
-					viewBox='0 0 24 24'
-				>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						strokeWidth='2'
-						d='M19 9l-7 7-7-7'
-					/>
-				</svg>
+				<div className='m-0 w-[80%] truncate p-0'>{value || placeholder}</div>
+				<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2'>
+					<svg
+						className='h-5 w-5'
+						fill='none'
+						stroke='currentColor'
+						viewBox='0 0 24 24'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth='2'
+							d='M19 9l-7 7-7-7'
+						/>
+					</svg>
+				</div>
 			</div>
+			{isOpen && (
+				<ul className='custom-dropdown__list'>
+					{options.map((option, index) => (
+						<li
+							key={index}
+							className='custom-dropdown__item'
+							onClick={() => handleSelect(option)}
+						>
+							{option}
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
